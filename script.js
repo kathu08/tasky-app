41113e8edd3537f3a7e67f5109a3e3718b14d73f
kathu@08 <sivalingamk.19@uom.lk>
2021-06-26 09:36:06 +0530

    add delete

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const taskContainer = document.querySelector(".task__container");
 
-const globalStore = [];
+let globalStore = [];
 
 const generateNewCard = (taskData) => `
-  <div class="col-md-6 col-lg-4" id=${taskData.id}>
+  <div class="col-md-6 col-lg-4">
     <div class="card">
       <div class="card-header d-flex justify-content-end gap-2">
         <button type="button" class="btn btn-outline-success">
           <i class="fas fa-pencil-alt"></i>
         </button>
-        <button type="button" class="btn btn-outline-danger">
-          <i class="fas fa-trash"></i>
+        <button type="button" class="btn btn-outline-danger" id=${taskData.id} onclick="deleteCard.apply(this, arguments)">
+          <i class="fas fa-trash-alt id=${taskData.id} onclick="deleteCard.apply(this, arguments)"></i>
         </button>
       </div>
       <img
@@ -43,12 +43,15 @@ const loadInitialCardData = () => {
 
   const { cards } = JSON.parse(getCardData);
 
-  //loop over those array of task object to create HTML card, inject it to DOM
+  //loop over those array of task object to create HTML card,
 
   cards.map((cardObject) => {
+    //inject it to DOM
+
     taskContainer.insertAdjacentHTML("beforeend", generateNewCard(cardObject));
 
     //update our globalStore
+
     globalStore.push(cardObject);
   });
 };
@@ -67,4 +70,31 @@ const saveChanges = () => {
   globalStore.push(taskData);
 
   localStorage.setItem("tasky", JSON.stringify({ cards: globalStore }));
+};
+
+const deleteCard = (event) => {
+  event = window.event;
+
+  //id
+  const targetID = event.target.id;
+  const tagname = event.target.tagName;
+
+  //match the id of the element with the id inside the globalStore
+  //if match found remove it
+
+  globalStore = globalStore.filter((cardObject) => cardObject.id !== targetID);
+
+  localStorage.setItem("tasky", JSON.stringify({cards:globalStore}));
+
+  //contact parent
+
+  if(tagname == "BUTTON") {
+    return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode);
+  }
+  else {
+    return taskContainer.removeChild(
+      event.target.parentNode.parentNode.parentNode.parentNode);
+  }
+
+ // taskContainer.removeChild(document.getElementById(targetID));
 };
